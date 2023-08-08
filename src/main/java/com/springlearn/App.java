b1a85831179a556619bec9f9eab8d5d17f18dec8
Sumit Kumar <sk4390789@gmail.com>
2023-08-08 12:26:16 +0530

    value as a attributes

--- src/main/java/com/springlearn/App.java
+++ src/main/java/com/springlearn/App.java
@@ -11,7 +11,9 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-        Student student = (Student) context.getBean("student1x");
-        System.out.println(student.getStudentName());
+        Student student1 = (Student) context.getBean("student1");
+        Student student2 = (Student) context.getBean("student2");
+        System.out.println(student1.toString());
+        System.out.println(student2.toString());
     }
 }
