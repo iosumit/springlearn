2408b5fdf05ba870baafd3fdac7d67e909f252e5
Sumit Kumar <sk4390789@gmail.com>
2023-08-10 12:18:36 +0530

    Stereo type - context:component-scan tag, @Component, @Value()

---
+++ src/main/java/com/springlearn/stereotype/StereoTest.java
@@ -0,0 +1,13 @@
+package com.springlearn.stereotype;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class StereoTest {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/springlearn/stereotype/stereoconfig.xml");
+
+        Student student = context.getBean("student", Student.class);
+        System.out.println(student);
+    }
+}
