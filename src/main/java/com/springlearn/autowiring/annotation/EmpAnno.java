f026d11e6e40b757afb16021779685c442afc25e
Sumit Kumar <sk4390789@gmail.com>
2023-08-10 11:03:11 +0530

    @Qualifier

--- src/main/java/com/springlearn/autowiring/annotation/EmpAnno.java
+++ src/main/java/com/springlearn/autowiring/annotation/EmpAnno.java
@@ -1,12 +1,14 @@
 package com.springlearn.autowiring.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class EmpAnno {
-    // @Autowired // -------Properties
+    @Autowired // -------Properties
+    @Qualifier("addressAnno2")
     private AddressAnno addressAnno;
 
-    // @Autowired // -------Properties
+    // @Autowired // -------Constructor
     public EmpAnno(AddressAnno addressAnno) {
         this.addressAnno = addressAnno;
         System.out.println("Constructor Injection");
@@ -19,7 +21,7 @@ public class EmpAnno {
         return addressAnno;
     }
 
-    @Autowired // -------Setter Injection
+    // @Autowired // -------Setter Injection
     public void setAddressAnno(AddressAnno addressAnno) {
         System.out.println("Setter Injection");
         this.addressAnno = addressAnno;
