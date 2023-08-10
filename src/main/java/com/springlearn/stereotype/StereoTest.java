6ef2b34bfdb0b14877cf7c7e10498625960fb95a
Sumit Kumar <sk4390789@gmail.com>
2023-08-10 12:49:44 +0530

    Bean Scopes singleton | prototype

--- src/main/java/com/springlearn/stereotype/StereoTest.java
+++ src/main/java/com/springlearn/stereotype/StereoTest.java
@@ -13,5 +13,17 @@ public class StereoTest {
         System.out.println(student.getVisited());
         System.out.println(student.getVisited().getClass().getName());
 
+        System.out.println(student.hashCode());
+        Student student2 = context.getBean("student", Student.class);
+        System.out.println(student2.hashCode());
+        // singleton 396883763 | 396883763
+        // prototype 1148254374 | 938545229
+
+        Teacher teacherObj1 = context.getBean("teacherObj", Teacher.class);
+        Teacher teacherObj2 = context.getBean("teacherObj", Teacher.class);
+
+        System.out.println(teacherObj1.hashCode());
+        System.out.println(teacherObj2.hashCode());
+
     }
 }
