900e36001602ece0c8c9fc1f404df9133bcd7884
Sumit Kumar <sk4390789@gmail.com>
2023-08-10 13:49:06 +0530

    spel - object, static method

--- src/main/java/com/springlearn/spel/Demo.java
+++ src/main/java/com/springlearn/spel/Demo.java
@@ -2,12 +2,46 @@ package com.springlearn.spel;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import java.lang.String;
 
 @Component
 public class Demo {
     @Value("#{20+33}")
     private int x;
     private int y;
+
+    @Value("#{ T(java.lang.Math).sqrt(100) }")
+    private int z;
+
+    @Value("#{ T(java.lang.Math).E }")
+    private double e;
+
+    @Value("#{ new java.lang.String('Sumit Kumar') }")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getE() {
+        return e;
+    }
+
+    public void setE(double e) {
+        this.e = e;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
 
     public int getX() {
         return x;
@@ -27,6 +61,7 @@ public class Demo {
 
     @Override
     public String toString() {
-        return "Demo [x=" + x + ", y=" + y + "]";
+        return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", e=" + e + ", name=" + name + "]";
     }
+
 }
